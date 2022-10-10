5ad3caeb7cc59a1f2a497ce4915d75420e0862e3
imber <1157911285@qq.com>
2022-10-11 00:15:27 +0800

    feat: node返回html

---
+++ next/src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>hello-ssr </h1>
+            <button onClick={() => {
+                alert('hello')
+            }}></button>
+        </div >
+    )
+}
+export default Home
